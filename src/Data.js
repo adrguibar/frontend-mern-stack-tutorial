d8038a1bdb33e777501e828c661430dc4aa4b119
adriano <adriano.a.barbosa@accenture.com>
2025-04-07 22:56:29 -0300

    criando a estrutura de paginas

---
+++ src/Data.js
@@ -0,0 +1,23 @@
+export const news = [
+    {
+        title: "Noticia 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticia 2",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticia 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 4
+    }
+]
